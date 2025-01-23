75ce05302e4c4093e454f7ae626ea388ef94ce3f
VicissitudeChat9619 <2223028651@qq.com>
2025-01-23 14:35:58 +0800

    采用了新的架构书写gui界面结构

--- main.py
+++ main.py
@@ -1,47 +1,33 @@
-# import sys
-# import random
-# from PySide6 import QtCore, QtWidgets, QtGui
-
-
-# class MyWidget(QtWidgets.QWidget):
-#     def __init__(self):
-#         super().__init__()
-
-#         self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-
-#         self.button = QtWidgets.QPushButton("Click me!")
-#         self.text = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
-
-#         self.layout = QtWidgets.QVBoxLayout(self)
-#         self.layout.addWidget(self.text)
-#         self.layout.addWidget(self.button)
-
-#         self.button.clicked.connect(self.magic)
-
-#     @QtCore.Slot()
-#     def magic(self):
-#         self.text.setText(random.choice(self.hello))
-
-
-# if __name__ == "__main__":
-#     app = QtWidgets.QApplication([])
-
-#     widget = MyWidget()
-#     widget.resize(800, 600)
-#     widget.show()
-
-#     sys.exit(app.exec())
 import sys
-from PySide6.QtGui import QGuiApplication
-from PySide6.QtQml import QQmlApplicationEngine
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
 
 if __name__ == "__main__":
-    app = QGuiApplication(sys.argv)
-    engine = QQmlApplicationEngine()
-    engine.addImportPath(sys.path[0])
-    engine.loadFromModule("Main", "Main")
-    if not engine.rootObjects():
-        sys.exit(-1)
-    exit_code = app.exec()
-    del engine
-    sys.exit(exit_code)
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
